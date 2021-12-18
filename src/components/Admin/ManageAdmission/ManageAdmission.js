20df979440778a9540f5d87dae29618560f17333
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-12-19 01:31:24 +0600

    working on payment

--- src/components/Admin/ManageAdmission/ManageAdmission.js
+++ src/components/Admin/ManageAdmission/ManageAdmission.js
@@ -2,11 +2,19 @@ import React,{useState, useEffect} from 'react';
 import AdminSideBar from '../AdminSideBar/AdminSideBar'
 const ManageAdmission = () => {
     const [admitStudents, setAdmitStudents] = useState([])
+    const [payments, setPayments] = useState([])
+
     useEffect(()=> {
         fetch('https://cryptic-stream-76661.herokuapp.com/onlineAdmission')
         .then(res => res.json())
         .then(data => setAdmitStudents(data))
     },[])
+    useEffect(()=> {
+        fetch('https://cryptic-stream-76661.herokuapp.com/payment')
+        .then(res => res.json())
+        .then(data => setPayments(data))
+    },[])
+
     return(
         <div className="row">
             <div className="col-md-3">
@@ -25,7 +33,7 @@ const ManageAdmission = () => {
                     <th scope="col">Department</th>
                     <th scope="col">SSC GPA</th>
                     <th scope="col">HSC GPA</th>
-                    {/* <th scope="col">Manage</th> */}
+                   
                     </tr>
                 </thead>
                 <tbody>
@@ -39,8 +47,38 @@ const ManageAdmission = () => {
                                 <td className="my-2 ">{registration.department}</td>
                                 <td className="my-2 ">{registration.sscGpa}</td>
                                 <td className="my-2 ">{registration.hscGpa}</td>
-                                {/* <button style={{color:'black'}} className="btn btn-success my-2">Accept</button>
-                                <button style={{color:'black'}} className="btn btn-danger my-2">Reject</button> */}
+                              
+                             </tr>   
+                        )
+                    }
+                </tbody>
+                </table>
+
+                <h2 style={{textAlign:'left', fontWeight:'700'}} className='mb-3 pt-3'>Payment</h2>
+                <table class="table">
+                <thead>
+                    <tr>
+                    
+                    <th scope="col">Phone</th>  
+                    <th scope="col">Trans Id</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Via</th>
+                    <th scope="col">Reference</th>
+                   
+                    
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        payments.map(payment => 
+                            <tr>
+                                <td className="my-2">{payment.phone}</td>
+                                <td className="my-2 ">{payment.transId}</td>
+                                <td className="my-2 ">{payment.amount}</td>
+                                <td className="my-2 ">{payment.method}</td>
+                                <td className="my-2 ">{payment.reference}</td>
+                                
+                              
                              </tr>   
                         )
                     }
