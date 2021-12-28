8770a96c8adf114a2b865a38c719bc3d3813d5eb
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-12-28 21:29:13 +0600

    email send successfully

--- src/components/Admin/SendEmail/SendEmail.js
+++ src/components/Admin/SendEmail/SendEmail.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
+import emailjs from 'emailjs-com'
 import AdminSideBar from '../AdminSideBar/AdminSideBar'
 import {useForm} from "react-hook-form";
 const SendEmail = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-
+    
     const onSubmit = (data) => {
+        
         const eventData = {
-            email: data.email,
+            email: data.email, 
+            name: data.name, 
             subject: data.subject,
             text: data.text
         }
+        console.log(eventData)
+        // e.preventDefault();
+        emailjs.send('service_0z2t2ze','template_nwuzhkm',eventData, 'user_jhLSFjXgv96rNW9HkCBaq')
+        .then(res => console.log(res))
+        .then(data => console.log(data))
+        alert('Email Send!');
 
-       console.log(eventData)
-       fetch('http://localhost:5000/email', {
-           method: 'POST',
-           headers : {'content-type': 'application/json'},
-           body : JSON.stringify(eventData)
-       })
-       .then(res => res.json())
-       .then(data => console.log(data))
-       alert('Email Try to send!')
     }
     return (
         <div className="row">
@@ -30,6 +30,8 @@ const SendEmail = () => {
             <h2 style={{fontWeight:'700', textAlign:'left'}} className="p-2">SEND EMAIL</h2>
                 <form className="form-group" onSubmit={handleSubmit(onSubmit)}>
                   
+                <input type='name' className='form-control' name='name' placeholder='Enter name' {...register("name", { required: true })} /> <br/>
+
                     <input type='email' className='form-control' name='email' placeholder='Enter Email' {...register("email", { required: true })} /> <br/>
                     <input type='text' className='form-control' name='subject' placeholder='Enter Subject' {...register("subject", { required: true })} /> <br/>
                     <textarea type='text' row='3' className='form-control' name='text' placeholder="Write Here..." {...register("text", { required: true })} /> <br />
